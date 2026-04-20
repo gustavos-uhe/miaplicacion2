29f8eb3981e2ec68edbe7d0dfc50733d3540ba9c
gustavos-uhe <gustavos@uhemisferios.edu.ec>
2026-04-20 17:34:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -21,3 +21,22 @@ col1, col2, col3 = st.columns(3)
 col1.metric("Ingresos", "$12K", "8%")
 col2.metric("Usuario", "1,204", "12%")
 col3.metric("Latencia", "42ms", "-3%")
+
+
+
+import streamlit as st
+import time
+
+tab1, tab2 = st.tabs(["Chart", "Data"], on_change="rerun")
+
+if tab1.open:
+    with st.spinner("Loading Tab 1..."):
+        time.sleep(2)
+    with tab1:
+        st.line_chart({"data": [1, 5, 2, 6]})
+
+if tab2.open:
+    with st.spinner("Loading Tab 2..."):
+        time.sleep(2)
+    with tab2:
+        st.dataframe({"col1": [1, 2, 3]})
